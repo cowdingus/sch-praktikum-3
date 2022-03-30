d1dee2d97b0fba8afdd99646d281fc699061787e
cowdingus <zaki.nadhif45@gmail.com>
2022-03-30 10:35:55 +0700

    Rewrite `App.jsx` and `main.jsx` to add react router structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,13 @@
-import Sidebar from "./components/Sidebar"
-import Main from "./pages/Main";
-import Cart from "./pages/Cart";
+import {Outlet} from "react-router-dom";
 
-import ayamgoyeng from "./assets/ayam-goreng.jpg";
-import ikanmasbumbukuning from "./assets/ikan-mas-bumbu-kuning.jpg";
-import ikanpari from "./assets/ikan-pari.jpg";
-import sateayam from "./assets/sate-ayam.jpg";
-import gulaikambing from "./assets/gulai-kambing.jpg";
-
-import {useState} from "react";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [menu, _setMenu] = useState([
-    {
-      image: ayamgoyeng,
-      title: "Ayam Goreng",
-      price: 8000,
-      rating: 4.5
-    },
-    {
-      image: ikanmasbumbukuning,
-      title: "Ikan Mas Bumbu Kuning",
-      price: 12000,
-      rating: 4.5
-    },
-    {
-      image: ikanpari,
-      title: "IKAN PARI ADALAH MAHKLUK HIDUP TIDAK UNTUK DIJUAL",
-      price: 999999999,
-      rating: 0
-    },
-    {
-      image: sateayam,
-      title: "Sate Ayam Pak Bambang",
-      price: 10000,
-      rating: 4.5
-    },
-    {
-      image: gulaikambing,
-      title: "Gulai Kambing",
-      price: 18000,
-      rating: 4.5
-    },
-  ]);
-
-  const [isCartOpen, _setIsCartOpen] = useState(true);
-  const [cart, setCart] = useState({});
-
-  const addIntoCart = (productTitle, quantity) => {
-    if (cart.hasOwnProperty(productTitle)) {
-      return setCart({...cart, [productTitle]: cart[productTitle] + (quantity ?? 1)});
-    }
-
-    setCart({...cart, [productTitle]: quantity ?? 1});
-  }
-
-  const takeOutFromCart = (productTitle, quantity) => {
-    quantity = quantity ?? 1;
-
-    if (cart.hasOwnProperty(productTitle)) {
-      if (cart[productTitle] - quantity > 0) {
-        return setCart({...cart, [productTitle]: cart[productTitle] - quantity});
-      }
-
-      const newCart = {...cart};
-      delete newCart[productTitle];
-
-      setCart(newCart);
-    }
-
-    console.warn("Item doesn't exist in cart to be decremented");
-  }
-
-  const getPriceOfMenu = (productTitle) => {
-    const index = menu.findIndex((item) => item.title === productTitle);
-
-    if (index === -1) {
-      console.warn("Product doesn't exist inside the menu for the price to be fetched");
-      return -1;
-    }
-
-    return menu[index].price;
-  }
-
   return (
-    <div className="flex items-stretch min-h-screen bg-stone-100">
-      <Sidebar menu={menu} cart={cart} />
-      <div className="flex flex-wrap items-stretch min-h-screen grow">
-        <Main menu={menu} cart={cart} addIntoCart={addIntoCart} />
-        {isCartOpen ? <Cart cart={cart} getPriceOfMenu={getPriceOfMenu} takeOutFromCart={takeOutFromCart}/> : ""}
+    <div className="flex flex-col items-stretch min-h-screen md:flex-row bg-stone-100">
+      <Sidebar />
+      <div className="flex flex-wrap items-stretch grow">
+        <Outlet />
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './index.css'
+
 import App from './App'
+import Shop from './pages/Shop';
+import EventList from './pages/EventList';
+import Index from './pages/Index'
 
 import 'remixicon/fonts/remixicon.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Index />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="event" element={<EventList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
