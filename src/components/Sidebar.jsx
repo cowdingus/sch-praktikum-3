99e781b4e21751cabe724ddec84da3169794fafa
cowdingus <zaki.nadhif45@gmail.com>
2022-03-30 10:36:26 +0700

    Add menu to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,18 @@
+import {Link} from "react-router-dom";
+
 import logo from "../assets/logo.svg";
 
-export default function Sidebar({ openAddForm }) {
+export default function Sidebar() {
   return (
-    <div className="hidden w-16 h-screen md:block">
-      <div className="fixed flex flex-col items-center w-16 h-screen py-3 bg-white border-r shadow border-stone-50 space-y-3">
+    <div className="z-10 w-screen h-16 md:w-16 md:h-screen">
+      <div className="fixed flex items-center justify-center w-screen h-16 py-3 text-2xl bg-white border-r shadow md:justify-start md:flex-col gap-6 md:w-16 md:h-screen border-stone-50 md:space-y-3 leading-3">
         <img src={logo} className="h-[40px]" />
+        <Link to="/event" className="order-first group md:order-none">
+          <i class="ri-file-list-3-line group-hover:bg-gray-200 rounded-sm p-2"></i>
+        </Link>
+        <Link to="/shop" className="group">
+          <i class="ri-shopping-basket-line group-hover:bg-gray-200 rounded-sm p-2"></i>
+        </Link>
       </div>
     </div>
   );
