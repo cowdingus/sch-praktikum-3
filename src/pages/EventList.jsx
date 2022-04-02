342c21a5b8d8925425f02071c4ed432925a1e64a
cowdingus <zaki.nadhif45@gmail.com>
2022-04-03 06:50:00 +0700

    Add default agendas and add clear button

--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -60,11 +60,45 @@ export default function EventList() {
 
   const [notes, noteController] = useNotes(new WebStorage("notes"));
 
+  function clearNotes() {
+    const shouldClear = window.confirm("Are you sure to clear all the agendas? It will be filled again with sample data if you do, but you need to refresh the page first");
+    if (shouldClear) noteController.clear();
+  }
+
+  if (noteController.read().length === 0) {
+    console.log("Array is empty, filling in with sample data");
+    noteController.add({ title: "Hari Peringatan Laut dan Samudera nasional", date: "2022-01-15"});
+    noteController.add({ title: "Hari Lahan Basah Sedunia", date: "2022-02-02" });
+    noteController.add({ title: "Hari Peduli Sampah Nasional", date: "2022-02-21" });
+    noteController.add({ title: "Hari Hutan Sedunia", date: "2022-03-21" });
+    noteController.add({ title: "Hari Air Sedunia", date: "2022-03-22" });
+    noteController.add({ title: "Hari Meteorologi Sedunia", date: "2022-03-23" });
+    noteController.add({ title: "Hari Bumi", date: "2022-04-22" });
+    noteController.add({ title: "Hari Burung Migratori Internasional", date: "2022-05-03" });
+    noteController.add({ title: "Hari Surya", date: "2022-05-03" });
+    noteController.add({ title: "Hari Biodiversitas Dunia", date: "2022-05-22" });
+    noteController.add({ title: "Hari Anti Tembakau Internasional", date: "2022-05-31" });
+    noteController.add({ title: "Hari Lingkungan Hidup Sedunia PBB", date: "2022-06-05" });
+    noteController.add({ title: "Hari Melawan Desertifikasi dan Kekeringan Dunia PBB", date: "2022-06-17" });
+    noteController.add({ title: "Hari Populasi Dunia PBB", date: "2022-07-11" });
+    noteController.add({ title: "Hari Perlindungan Lapisan Ozon Sedunia", date: "2022-09-16" });
+    noteController.add({ title: "Hari Emisi Nol", date: "2022-09-20" });
+    noteController.add({ title: "Hari Bebas Mobil", date: "2022-09-22" });
+  }
+
   return (
     <React.Fragment>
       {popUpStack}
       <main className="p-6 grow">
-        <h1 className="text-2xl font-bold">Agenda Hari Lingkungan</h1>
+        <div className="flex flex-wrap items-center">
+          <h1 className="text-2xl font-bold grow">Agenda Hari Lingkungan</h1>
+          <button 
+            className="fixed flex items-center justify-center md:static bottom-8 left-8 w-12 h-12 md:w-10 md:h-10 text-4xl md:text-2xl bg-white rounded-full shadow hover:scale-[1.07] transition-transform flex-wrap z-10"
+            onClick={clearNotes}
+          >
+            <i className="ri-delete-bin-line"></i>
+          </button>
+        </div>
         <Masonry
           breakpointCols={{
             default: 5,
@@ -72,17 +106,20 @@ export default function EventList() {
             700: 3,
             500: 1,
           }}
-          className="flex mt-4 gap-5 grow"
+          className="flex mt-4 gap-4 grow"
           columnClassName=""
         >
           {notes.map((note) => (
-            <NoteCard
-              key={note.id}
-              note={note}
-              handleClick={openEditNoteForm}
-            />
+            <div className="mb-4">
+              <NoteCard
+                key={note.id}
+                note={note}
+                handleClick={openEditNoteForm}
+              />
+            </div>
           ))}
         </Masonry>
+        <div className="h-14 md:hidden"></div>
         <button 
           className="fixed flex items-center justify-center w-12 h-12 text-4xl bg-white rounded-full shadow bottom-8 right-8 hover:scale-[1.07] transition-transform"
           onClick={openAddNoteForm}
